6920d461be98baf04f5bb9c0099c475740a9fd72
cdapayne <carlton.payne@hotmail.com>
2025-08-29 20:15:07 -0400

    update

---
+++ RowFit/RowFitTests/RowFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  RowFitTests.swift
+//  RowFitTests
+//
+//  Created by Carlton Payne III on 8/29/25.
+//
+
+import Testing
+@testable import RowFit
+
+struct RowFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
